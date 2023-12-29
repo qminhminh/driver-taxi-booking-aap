b6c90d76766965be640115d2408dc3be58a6c35a
qminhminh <hqminh050503@gmail.com>
2023-12-29 11:33:37 +0700

    fix chat 2

--- lib/features/chat/screens/chat_screen.dart
+++ lib/features/chat/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print, no_leading_underscores_for_local_identifiers, unused_field, library_private_types_in_public_api
+
 import 'dart:async';
 import 'package:driver_taxi_booking_app/common/widgets/loader.dart';
 import 'package:driver_taxi_booking_app/features/chat/services/chat_services.dart';
@@ -11,8 +13,13 @@ import 'package:provider/provider.dart';
 import 'package:socket_io_client/socket_io_client.dart';
 
 class ChatScreen extends StatefulWidget {
-  const ChatScreen(
-      {super.key, required this.name, required this.image, required this.id});
+  const ChatScreen({
+    Key? key,
+    required this.name,
+    required this.image,
+    required this.id,
+  }) : super(key: key);
+
   final String name;
   final String image;
   final String id;
@@ -27,187 +34,263 @@ class _ChatScreenState extends State<ChatScreen> {
   final _socketClient = SocketClient.internal.socket!;
   Socket get socketClient => _socketClient;
   List<Message>? _list;
-  late Timer _timer;
   final ChatServices chatServices = ChatServices();
+  final StreamController<List<Message>> _messageStreamController =
+      StreamController<List<Message>>();
 
   @override
   void dispose() {
     super.dispose();
     textController.dispose();
-    _timer.cancel();
+    _messageStreamController.close();
   }
 
   fecthAllMessage() async {
     _list = await chatServices.getListMessages(context, widget.id);
-
-    setState(() {});
+    _messageStreamController.add(_list!);
   }
 
   @override
   void initState() {
     super.initState();
     fecthAllMessage();
-
-    _timer = Timer.periodic(const Duration(seconds: 1), (Timer timer) {
-      fetchAndSetListMessages();
-    });
+    _setupSocketListeners();
   }
 
-  void fetchAndSetListMessages() async {
-    final messages = await chatServices.getListMessages(context, widget.id);
-    setState(() {
-      _list = messages;
+  _setupSocketListeners() {
+    // Listen to the "feedbackserver" event
+    socketClient.on('feedbackserver', (data) {
+      print('Received feedback from server: $data');
+      _refreshMessages();
     });
   }
 
   Future<void> _refreshMessages() async {
     _list = await chatServices.getListMessages(context, widget.id);
-
-    setState(() {});
+    _messageStreamController.add(_list!);
   }
 
   @override
   Widget build(BuildContext context) {
-    final userprovider = Provider.of<UserProvider>(context, listen: false).user;
+    final userprovider = Provider.of<UserProvider>(context, listen: false);
 
     return Scaffold(
       appBar: AppBar(
-        leading: ClipOval(
-          child: Image.network(
-            widget.image == ''
-                ? "https://firebasestorage.googleapis.com/v0/b/taxi-booking-acb4b.appspot.com/o/Images%2Favatar.png?alt=media&token=fcd3d7f8-6915-440b-a617-d258e8a3fd15"
-                : widget.image,
-            width: 30,
-            height: 30,
-            fit: BoxFit.cover,
+        leading: GestureDetector(
+          onTap: () async {
+            await _refreshMessages();
+          },
+          child: ClipOval(
+            child: Image.network(
+              widget.image == ''
+                  ? "https://firebasestorage.googleapis.com/v0/b/taxi-booking-acb4b.appspot.com/o/Images%2Favatar.png?alt=media&token=fcd3d7f8-6915-440b-a617-d258e8a3fd15"
+                  : widget.image,
+              width: 30,
+              height: 30,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         title: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                widget.name,
-                style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-              ),
-              Text(
-                widget.id,
-                style: const TextStyle(fontSize: 13),
-              ),
-            ]),
-      ),
-      body: RefreshIndicator(
-        onRefresh: _refreshMessages,
-        child: Column(children: [
-          Expanded(
-            child: _list == null
-                ? const Loader()
-                : _list!.isEmpty
-                    ? const Center(child: Text('No messages available'))
-                    : ListView.builder(
-                        itemCount: _list!.length,
-                        reverse: true,
-                        padding: const EdgeInsets.only(top: 10),
-                        physics: const BouncingScrollPhysics(),
-                        itemBuilder: (context, intdex) {
-                          return MessageCard(
-                            message: _list![intdex],
-                          );
-                        }),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    child: Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            setState(() {
-                              FocusScope.of(context).unfocus();
-                            });
-                          },
-                          icon: const Icon(
-                            Icons.emoji_emotions,
-                            color: Colors.blueAccent,
-                            size: 25,
-                          ),
-                        ),
-                        Expanded(
-                            child: TextField(
-                          controller: textController,
-                          keyboardType: TextInputType.multiline,
-                          maxLines: null,
-                          onTap: () {},
-                          decoration: const InputDecoration(
-                              hintText: 'Type...',
-                              hintStyle: TextStyle(color: Colors.blueAccent),
-                              border: InputBorder.none),
-                        )),
-                        IconButton(
-                          onPressed: () {
-                            setState(() {});
-                          },
-                          icon: const Icon(
-                            Icons.image,
-                            color: Colors.blueAccent,
-                            size: 26,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            setState(() {});
-                          },
-                          icon: const Icon(
-                            Icons.camera_alt_rounded,
-                            color: Colors.blueAccent,
-                            size: 26,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                MaterialButton(
-                  onPressed: () {
-                    if (textController.text.isNotEmpty) {
-                      // create chat
-                      _socketMethods.createRoomChat(
-                          textController.text, userprovider.id, widget.id);
-
-                      // feaaback server
-
-                      Future.delayed(const Duration(seconds: 1), () {
-                        fecthAllMessage();
-                        setState(() {
-                          fecthAllMessage();
-                        });
-                      });
-
-                      textController.text = '';
-                    }
-                  },
-                  minWidth: 0,
-                  padding: const EdgeInsets.only(
-                      top: 10, bottom: 10, right: 5, left: 10),
-                  shape: const CircleBorder(),
-                  color: Colors.lightBlueAccent,
-                  child: const Icon(
-                    Icons.send,
-                    color: Colors.black,
-                    size: 28,
-                  ),
-                ),
-              ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.name,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
             ),
-          ),
-        ]),
+            Text(
+              widget.id,
+              style: const TextStyle(fontSize: 13),
+            ),
+          ],
+        ),
+      ),
+      body: _buildSocketListener(userprovider.user.id),
+    );
+  }
+
+  Widget _buildSocketListener(String proid) {
+    return SocketListener(
+      onReceive: (data) {
+        print('Received feedback from server inside SocketListener: $data');
+        _refreshMessages();
+      },
+      child: RefreshIndicator(
+        onRefresh: _refreshMessages,
+        child: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder<List<Message>>(
+                stream: _messageStreamController.stream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    List<Message>? messages = snapshot.data;
+                    return _buildMessageList(messages);
+                  } else {
+                    return const Loader();
+                  }
+                },
+              ),
+            ),
+            _buildInputArea(proid),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _buildMessageList(List<Message>? messages) {
+    return _list == null
+        ? const Loader()
+        : _list!.isEmpty
+            ? const Center(child: Text('No messages available'))
+            : ListView.builder(
+                itemCount: _list!.length,
+                reverse: true,
+                padding: const EdgeInsets.only(top: 10),
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, intdex) {
+                  return MessageCard(
+                    message: _list![intdex],
+                  );
+                },
+              );
+  }
+
+  Widget _buildInputArea(String userId) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Row(
+        children: [
+          Expanded(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        FocusScope.of(context).unfocus();
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.emoji_emotions,
+                      color: Colors.blueAccent,
+                      size: 25,
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: textController,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      onTap: () {},
+                      decoration: const InputDecoration(
+                        hintText: 'Type...',
+                        hintStyle: TextStyle(color: Colors.blueAccent),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {});
+                    },
+                    icon: const Icon(
+                      Icons.image,
+                      color: Colors.blueAccent,
+                      size: 26,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {});
+                    },
+                    icon: const Icon(
+                      Icons.camera_alt_rounded,
+                      color: Colors.blueAccent,
+                      size: 26,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          MaterialButton(
+            onPressed: () {
+              _sendMessage(userId);
+            },
+            minWidth: 0,
+            padding: const EdgeInsets.only(
+              top: 10,
+              bottom: 10,
+              right: 5,
+              left: 10,
+            ),
+            shape: const CircleBorder(),
+            color: Colors.lightBlueAccent,
+            child: const Icon(
+              Icons.send,
+              color: Colors.black,
+              size: 28,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _sendMessage(String userId) async {
+    if (textController.text.isNotEmpty) {
+      _socketMethods.createRoomChat(
+        textController.text,
+        userId,
+        widget.id,
+      );
+
+      // Không cần _refreshMessages() ở đây vì đã có lắng nghe sự kiện feedbackserver
+
+      textController.text = '';
+    }
+  }
+}
+
+class SocketListener extends StatefulWidget {
+  final Widget child;
+  final void Function(dynamic data) onReceive;
+
+  const SocketListener({Key? key, required this.child, required this.onReceive})
+      : super(key: key);
+
+  @override
+  _SocketListenerState createState() => _SocketListenerState();
+}
+
+class _SocketListenerState extends State<SocketListener> {
+  late final SocketMethods _socketMethods;
+
+  @override
+  void initState() {
+    super.initState();
+    _socketMethods = SocketMethods();
+    _setupSocketListeners();
+  }
+
+  _setupSocketListeners() {
+    final _socketClient = SocketClient.internal.socket;
+
+    if (_socketClient != null) {
+      _socketClient.on('feedbackserver', (data) {
+        widget.onReceive(data);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
   }
 }
